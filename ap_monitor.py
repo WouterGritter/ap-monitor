9612b7fc65db73da9986bd3f19b60c77677adda5
Wouter Gritter <wouter@gritter.me>
2024-04-26 16:54:47 +0200

    Initiate recovery after RECOVERY_ATTEMPT_DELAY

--- ap_monitor.py
+++ ap_monitor.py
@@ -1,11 +1,16 @@
+import os
+import time
 from typing import Optional
 
 from logger.logger import Logger
 from tplink_switch import TpLinkSwitch
 from utils import is_host_reachable
 
+RECOVERY_ATTEMPT_DELAY = os.getenv('RECOVERY_ATTEMPT_DELAY', 30 * 60)
+
 
 class APMonitor:
+
     def __init__(self, host: str, name: str, switch: TpLinkSwitch, switch_port: int, logger: Logger):
         self.host = host
         self.name = name
@@ -14,6 +19,7 @@ class APMonitor:
         self.logger = logger
 
         self.switch_port_disabled = False
+        self.perform_recovery_at = None
 
     async def perform_check(self):
         if not await self.switch.is_reachable():
@@ -23,8 +29,9 @@ class APMonitor:
         if reachable:
             if self.switch_port_disabled:
                 self._log(f'AP is reachable again after disabling switch port. '
-                      f'Assuming anomaly has been fixed and the port has been enabled manually.')
+                          f'Assuming anomaly has been fixed and the port has been enabled manually.')
                 self.switch_port_disabled = False
+                self.perform_recovery_at = None
         else:
             if not self.switch_port_disabled:
                 self._log(f'AP is not reachable. Assuming someone has tampered with the AP connection. '
@@ -35,8 +42,12 @@ class APMonitor:
                 try:
                     await self.switch.set_port_enabled(self.switch_port, False)
                     self.switch_port_disabled = True
+                    self.perform_recovery_at = time.time() + RECOVERY_ATTEMPT_DELAY
                 except Exception as e:
                     self._log(f'An exception occurred when attempting to disable the port. Retrying on next check. {e}')
+            else:
+                if self.perform_recovery_at is not None and time.time() >= self.perform_recovery_at:
+                    await self.attempt_recovery()
 
     async def attempt_recovery(self):
         if not await self.switch.is_reachable():
@@ -46,14 +57,16 @@ class APMonitor:
 
         await self.switch.set_port_enabled(self.switch_port, True)
 
-        reachable = await self.is_reachable(30)
+        reachable = await self.is_reachable(5)
         if reachable:
             self._log('Recovery successful, AP is reachable again.')
             self.switch_port_disabled = False
+            self.perform_recovery_at = None
         else:
             self._log('Recovery unsuccessful. AP is not reachable after enabling port.')
             await self.switch.set_port_enabled(self.switch_port, False)
             self.switch_port_disabled = True
+            self.perform_recovery_at = time.time() + RECOVERY_ATTEMPT_DELAY
 
     def _log(self, text: str):
         self.logger.log(f'[{self.name}@{self.host}] {text}')
